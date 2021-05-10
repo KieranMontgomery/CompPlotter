d79b5177926ffa02b3658e8e493ed64e7ebce145
KieranMontgomery <k.montgomery1@hotmail.co.uk>
2021-05-10 23:23:25 +0100

    Timing

--- CompPlotter/src/CompPlotter.cpp
+++ CompPlotter/src/CompPlotter.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
 #include <vector>
 
+#include <chrono>
+
 #include "../include/Surfdata.h"
+
+#define ITER_BENCH 100
 
 int main()
 {
     std::string filepath = "data/forces820000.dat";
-    Surfdata s(filepath);
 
-
-    for (size_t i = 0; i < 10; i++)
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    for (size_t i = 0; i < ITER_BENCH; i++)
     {
-        std::cout << s.points[i] << std::endl;
+        Surfdata s(filepath); /* code */
     }
     
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count()/ITER_BENCH << "[µs]" << std::endl;
 
-
+    filepath = "D:/Users/kmont/Documents/LongJet/forces820000.dat";
+    begin = std::chrono::steady_clock::now();
+    for (size_t i = 0; i < ITER_BENCH; i++)
+    {
+        Surfdata s(filepath); /* code */
+    }
+    end = std::chrono::steady_clock::now();
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count()/ITER_BENCH << "[µs]" << std::endl; 
 }
--- CompPlotter/src/DataReader.cpp
+++ CompPlotter/src/DataReader.cpp
@@ -13,11 +13,13 @@ std::vector<std::vector<double>> extractDataFromFile(std::string& filename)
 
     std::string line;
     std::vector<std::vector<double>> output;
+    std::vector<double> v;
+    v.reserve(25);
     while(getline(file, line))
     {
+        v.clear();
         line = line.substr(line.find_first_not_of(" "));
         size_t pos = 0;
-        std::vector<double> v;
         while ((pos = line.find("  ")) != std::string::npos) {
             v.push_back(std::stod(line.substr(0, pos)));
             line.erase(0, pos + 2); // pos plus delimeter size
